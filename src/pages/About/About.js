36a070c051eb115926f6b22b827140e729fb9495
Md. Nuray Alam Siddik <siddik4x@gmail.com>
2023-02-05 17:04:20 +0600

    about, contact page content added

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import LinkButton from '../../components/ui/LinkButton';
 
 const About = () => {
     return (
         <div>
-            <h2>This is About Page</h2>
+            <div className="hero min-h-screen" style={{ backgroundImage: `url("https://i.ibb.co/x63cBK5/about.png")` }}>
+                <div className="hero-overlay bg-opacity-90"></div>
+                <div className="hero-content text-center text-neutral-content">
+                    <div className="max-w-md">
+                        <h1 className="mb-5 text-5xl text-accent font-bold">Hello there!</h1>
+                        <h1 className="mb-5 text-5xl text-accent font-bold"> We are a very Renowned AI Solution Provider</h1>
+                        <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        <LinkButton link='/contact'>Get Started</LinkButton>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
